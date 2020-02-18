01a1d406c789fa661822005274f12a67b93d506a
SimoesFO <felipeoliveirasimoes@hotmail.com>
2020-02-17 23:05:27 -0300

    Mais algumas funções

--- codesReferences/JSArrayIterator/script.js
+++ codesReferences/JSArrayIterator/script.js
@@ -1,24 +1,8 @@
 data = [
-    {
-        name: 'Butters',
-        age: 3,
-        type: 'dog'
-    },
-    {
-        name: 'Lizzy',
-        age: 6,
-        type: 'dog'
-    },
-    {
-        name: 'Red',
-        age: 1,
-        type: 'cat'
-    },
-    {
-        name: 'Joey',
-        age: 3,
-        type: 'dog'
-    },
+    { name: 'Butters', age: 3, type: 'dog' },
+    { name: 'Lizzy', age: 6, type: 'dog' },
+    { name: 'Red', age: 1, type: 'cat' },
+    { name: 'Joey', age: 3, type: 'dog' },
 ];
 
 
@@ -68,6 +52,15 @@ let booleanAge = data.every(animal => {
 console.log(booleanAge);
 
 
+// Retorna Verdadeiro se PELO MENOS UM dos objetos satisfazer a condição da função.
+// Se houve pelo menos um gato retorna verdadeiro.
+console.log("SOME")
+let hasCat = data.some( animal => {
+    return animal.type === 'cat'
+})
+console.log(hasCat);
+
+
 // Soma todas as idades dos animais.
 console.log("REDUCE")
 let sumAge = data.reduce( (accumulator, animal) => {
@@ -83,3 +76,14 @@ let result = data
     .filter( age => (age > 10) )
     .reduce( (accumulator, currentValue) => accumulator + currentValue )
 console.log(result)
+
+
+// Verifica se um array contém um determinado elemento. Este array não pode ser um objeto,
+// apenas um array simples.
+console.log("INCLUDES")
+let newArray = [1, 2, 3, 4, 5]
+let hasContent = newArray.includes(2);
+console.log(hasContent);
+// Verifica se existe o n° 2 a partir do 4° elemento do array.
+hasContent = newArray.includes(2, 3);
+console.log(hasContent);
