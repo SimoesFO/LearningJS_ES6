5a2350e47f58418c22f735e4624579315bfccefb
unknown <ti7@orthodonticbrasil.com.br>
2020-07-26 20:50:34 -0300

    Finishing Project

--- smallProjects/JSRepositoriesGitHub/app.js
+++ smallProjects/JSRepositoriesGitHub/app.js
@@ -1,31 +1,87 @@
 class App {
+
   constructor() {
     this.repositories = []
     
     this.formEl = document.getElementById('repo-form')
+    this.inputEl = document.querySelector('input[name=repository]')
     this.listEl = document.getElementById('repo-list')
+    this.buttonEl = document.querySelector('button[type=submit]')
 
     this.registerHandlers();
   }
+
 
   registerHandlers() {
     this.formEl.onsubmit = event => this.addRepository(event)
   }
 
-  addRepository(event) {
+
+  async addRepository(event) {
     event.preventDefault()
 
-    this.repositories.push({
-      name: 'rocketset.com.br',
-      description: 'Tire a sua ideia do papel',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'http://github.com/rocketseat/rocketseat.com.br'
-    })
+    try {
+      const repoInput = this.inputEl.value
 
-    this.render()
+      if(repoInput.length === 0) return
+
+      this.setLoading();
+
+      let response = await this.getDataGitHub(repoInput)
+
+      const { name, html_url, description, blog, owner: { avatar_url } = {}, avatar_url: avatar_url_user } = response
+
+      let avatar = avatar_url || avatar_url_user 
+      let content = description || blog
+
+      this.repositories.push({
+        name,
+        description: content,
+        avatar_url: avatar,
+        html_url
+      })
+
+      this.render()
+    }
+    catch(error) {
+      alert('Repository not found')
+    }
+
+    this.setLoading(false);
   }
 
+
+  getDataGitHub(repository) {
+    
+    let BASE_URL = 'https://api.github.com/'
+    
+    if( repository.includes('/') ) {
+      BASE_URL += 'repos/'
+    }
+    else {
+      BASE_URL += 'users/'
+    }
+
+    return fetch(BASE_URL+repository)
+        .then(response => response.json())
+        .then(data => data)
+  }
+
+
+  setLoading(loading = true) {
+    if( loading === true) {
+      this.buttonEl.innerHTML = 'Carregando...'
+      this.buttonEl.disabled = true
+    }
+    else {
+      this.buttonEl.innerHTML = 'Adicionar'
+      this.buttonEl.disabled = false
+    }
+  }
+
+
   render() {
+    this.inputEl.value = ''
     this.listEl.innerHTML = ''
 
     this.repositories.forEach(repo => {
